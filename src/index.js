bfc2c173d542f6b4fd8780d87d20d186d05f1886
AP PH <ahphu@ucsd.edu>
2018-12-28 17:23:44 -0800

    Moved state to board component

--- src/index.js
+++ src/index.js
@@ -4,29 +4,35 @@ import './index.css';
 
 class Square extends React.Component {
 
-  // Square's private "state"
-  constructor(props) {
-    super(props);
-    this.state = {
-        value: null,
-    };
-  }
-
   render() {
     return (
       <button 
         className="square" 
-        onClick={() => this.setState({value: 'X'})}
+        onClick={() => this.props.onClick({value: 'X'})}
       >
-        {this.state.value}
+        {this.props.value}
       </button>
     );
   }
 }
 
 class Board extends React.Component {
+
+  // Board's private state ( 9 squares )
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+    };
+  }
+
   renderSquare(i) {
-    return <Square value={i} />;
+    return ( 
+      <Square 
+        value = { this.state.squares[i] }
+        onClick = { () => this.handleClick(i) }
+      />
+    );
   }
 
   render() {
